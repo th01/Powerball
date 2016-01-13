4accea871b2d0f7207f639d8015ac4b1657dc90b
Travis Hooper <travis.d.hooper@gmail.com>
2016-01-12 22:49:12 -0600

    Reworked script

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-A small little script to create a SQLite3 database containing all of the possible outcomes from [Powerball](http://www.powerball.com).
+A small little script that I put together for fun to create a SQLite3 database containing all of the possible outcomes from [Powerball](http://www.powerball.com).
 
-The script is able to populate a database at ~100,000 combinations per second and complete the script for all
-292 million combinations in ~49 minutes.
+The script is able to populate a database with all 292 million combinations in ~50 minutes.
--- powerball.rb
+++ powerball.rb
@@ -1,64 +1,91 @@
 require 'sqlite3'
 
-%x(rm powerball.db)
+module GetPowerballCombos
+  module Database
+    def self.rm_existing_db
+      %x(rm powerball.db)
+    end
 
-DB = SQLite3::Database.new 'powerball.db'
+    def self.create_db
+      @db = SQLite3::Database.new 'powerball.db'
+    end
 
-result = DB.execute <<-SQL
-  CREATE TABLE numbers (
-    one       INT,
-    two       INT,
-    three     INT,
-    four      INT,
-    five      INT,
-    powerball INT
-  );
-SQL
+    def self.create_table
+      db.execute <<-SQL
+        CREATE TABLE numbers(
+          id        INTEGER PRIMARY KEY AUTOINCREMENT,
+          one       INT,
+          two       INT,
+          three     INT,
+          four      INT,
+          five      INT,
+          powerball INT
+        );
+      SQL
+    end
 
-# (one, two, three, four, five, powerball)
+    def self.db
+      @db
+    end
+  end
 
-def insert_combs_into_db(combos)
-  inserts = combos.map { |combo| "(#{combo.join(',')})" }.join(",\n")
-  sql = <<-SQL
-    INSERT INTO numbers VALUES
-    #{inserts};
-  SQL
-  DB.execute sql
-end
+  Database.rm_existing_db
+  Database.create_db
+  Database.create_table
+  DB              = Database.db
+  BALLS           = (1..69).to_a
+  POWERBALLS      = (1..26).to_a
+  START           = Time.now
+  INSERT_INTERVAL = 500
 
-def string_number(n)
-  n.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-end
+  def self.find_combos
+    combos       = []
+    counter      = 0
+    last_counter = 0
+    last_time    = START
 
-BALLS           = (1..69).to_a
-POWERBALLS      = (1..26).to_a
-START           = Time.now
-INSERT_INTERVAL = 500
-last            = START
-counter         = 0
-combos          = []
-
-BALLS.each do |b1|
-  BALLS[b1..-1].each do |b2|
-    BALLS[b2..-1].each do |b3|
-      BALLS[b3..-1].each do |b4|
-        BALLS[b4..-1].each do |b5|
-          POWERBALLS.each do |pb|
-            combo = [b1, b2, b3, b4, b5, pb]
-            combos << combo
-            counter += 1
-            if (counter % INSERT_INTERVAL).zero?
-              now = Time.now
-              insert_combs_into_db(combos)
-              combos = []
-              puts "#{string_number(counter)} combos processed at #{(INSERT_INTERVAL/(now - last)).to_i} combos/s"
-              last = now
+    BALLS.each do |b1|
+      BALLS[b1..-1].each do |b2|
+        BALLS[b2..-1].each do |b3|
+          BALLS[b3..-1].each do |b4|
+            BALLS[b4..-1].each do |b5|
+              POWERBALLS.each do |pb|
+                combo = [b1, b2, b3, b4, b5, pb]
+                combos << combo
+                counter += 1
+                if (counter % INSERT_INTERVAL).zero? || (b5 == BALLS.last && pb == POWERBALLS.last)
+                  now = Time.now
+                  insert_combos_into_db(combos)
+                  combos = []
+                  puts "#{string_number(counter)} combos processed at #{((counter - last_counter)/(now - last_time)).to_i} combos/s"
+                  last_time    = now
+                  last_counter = counter
+                end
+              end
             end
           end
         end
       end
     end
+    puts "#{string_number(counter)} combos processed in #{Time.now - START} seconds"
+  end
+
+  private
+
+  def self.insert_combos_into_db(combos)
+    inserts = combos.map { |combo| "(#{combo.join(',')})" }.join(",\n")
+    sql = <<-SQL
+      INSERT INTO numbers (one, two, three, four, five, powerball)
+      VALUES
+      #{inserts};
+    SQL
+    DB.execute sql
+  end
+
+  def self.string_number(n)
+    n.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
 end
 
-puts counter
+
+GetPowerballCombos.find_combos
